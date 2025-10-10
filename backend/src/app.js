ab9c14bcc65095f6d5b63e221b852573c5ea2662
dimasqr <dimasquintana1@gmail.com>
2025-10-10 08:35:15 -0600

    Temp changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,17 +3,13 @@ import dotenv from 'dotenv';
 import errorHandler from './middleware/errorHandler.js';
 import db from './models/index.js';
 import { initRedis } from './utils/redisClient.js';
-import categoryRoutes from './routes/category.js';
-import productRoutes from './routes/product.js';
-import roleRoutes from './routes/role.js';
-import userRoutes from './routes/user.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use("/api", authRoutes);
 
-/*
 app.get('/', (req, res) => {
     res.json({ message: 'Backend running' });
 });
@@ -30,17 +26,29 @@ app.use(errorHandler);
 const startServer = async () => {
     try {
         await initRedis();
+
+        const categoryRoutes = (await import('./routes/category.js')).default;
+        const productRoutes = (await import('./routes/product.js')).default;
+
+        app.use('/api/categories', categoryRoutes);
+        app.use('/api/products', productRoutes);
+
         db.sequelize.sync( { alter: true } ); //warning: 'alter' updates tables, 'force' deletes and recreates
+
+        // Error middleware (always at the end)
+        app.use(errorHandler);
+
         app.listen(process.env.PORT, '0.0.0.0', () => {
             console.log(`Backend running on port ${process.env.PORT}`);
         });
+
     } catch (err) {
         console.error('Error during initialization: ', err);
     }
 };
 
-if(process.env.NODE_ENV !== 'test'){
-    startServer();
+if (process.env.NODE_ENV !== "test") {
+  startServer();
 }
 
 export default app;
